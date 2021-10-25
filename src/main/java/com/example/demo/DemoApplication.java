f451f8d4851145feb864aa7f3aa8435c1b32465c
Adam Stewart <adamstewart94@gmail.com>
2021-10-25 11:03:32 +0000

    test 123 post added
    new test mapping

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,5 +17,10 @@ public class DemoApplication {
     @GetMapping("/hello-spring")
     String greet(){
         return "Hello, Spring!";
+    }
+
+    @PostMapping("/test")
+    String test(){
+        return "Hello, test!";
     }
 }
